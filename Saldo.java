08755835fb6a77e85520199132ce9a0c865abcd2
linkherz <linkherzg@gmail.com>
2016-11-25 22:43:17 +0700

    saldo

---
+++ Saldo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package saldo;
+
+/**
+ *
+ * @author UsuiTakumi
+ */
+public class Saldo {
+    
+    private SaldoDB jumlahsaldo;
+    
+    public Saldo(){
+        jumlahsaldo = new SaldoDB();
+    }
+    
+    public boolean tambah(int saldo){
+        return true;
+    }
+    
+    public void lihat(){
+        
+    }
+}

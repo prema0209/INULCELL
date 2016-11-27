479105145c8cc2a61487e786641a2781bd3a7fdd
prema0209 <igustiagungpremananda@gmail.com>
2016-11-27 22:56:11 +0700

    Update CpControl.java

--- CpControl.java
+++ CpControl.java
@@ -11,30 +11,26 @@ package cpagen;
  * @author UsuiTakumi
  */
 public class CpControl {
-    private CPagenDB data;
+    
     
     public CpControl(){
-        data = new CPagenDB();
+        
     }
     
-    /*  Yang penting User telah memasukkan nama.
-        Apabila User telah memasukkan nama dan no hp -> true
-        Apabila User telah memasukkan nama tanpa no hp -> true
-        Apabila User telah memasukkan no hp tanpa nama -> false
-        Apabila User tidak memasukkan nama dan no hp -> false
-    */
     public boolean tambah(String nama, String NoHp) {
-        return !"".equals(nama);//||!"".equals(NoHp);
+        
+        
+        
+        
+        return true;
+        
     }
     
     public void lihat(){
         
     }
     
-    /*  Yang penting User telah memasukkan apa pun di text nama
-        Apabila User tidak memasukkan nama ("") -> false
-    */
     public boolean hapus(String nama){
-        return !"".equals(nama);
+        return true;
     } 
 }

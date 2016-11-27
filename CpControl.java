047cef5295b6510d0454af2360ff510115212b6c
linkherz <linkherzg@gmail.com>
2016-11-27 23:55:39 +0700

    Update CpControl.java
    ku sesuai kan sama punyaku lagi dan ada comment. supaya, kalau kosong, gk bisa di tambahkan

--- CpControl.java
+++ CpControl.java
@@ -12,25 +12,28 @@ package cpagen;
  */
 public class CpControl {
     
-    
     public CpControl(){
         
     }
     
+    /*  Yang penting User telah memasukkan nama.
+        Apabila User telah memasukkan nama dan no hp -> true
+        Apabila User telah memasukkan nama tanpa no hp -> true
+        Apabila User telah memasukkan no hp tanpa nama -> false
+        Apabila User tidak memasukkan nama dan no hp -> false
+    */
     public boolean tambah(String nama, String NoHp) {
-        
-        
-        
-        
-        return true;
-        
+        return !"".equals(nama);//||!"".equals(NoHp);
     }
     
     public void lihat(){
         
     }
     
+    /*  Yang penting User telah memasukkan apa pun di text nama
+        Apabila User tidak memasukkan nama ("") -> false
+    */
     public boolean hapus(String nama){
-        return true;
+        return !"".equals(nama);
     } 
 }

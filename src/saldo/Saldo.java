2c8d9da4b65cfdc96f3d2f84d1a45f9ee67f2ed7
linkherz <linkherzg@gmail.com>
2016-12-03 00:03:37 +0700

    Update Saldo.java
    masukkan link DB, taruh try catch

--- src/saldo/Saldo.java
+++ src/saldo/Saldo.java
@@ -6,15 +6,45 @@
 
 package saldo;
 
+import cpagen.CPagenDB;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 /**
  *
  * @author UsuiTakumi
  */
 public class Saldo {
     
+    Connection connection=null;
+    Statement stmt;
+    ResultSet rsSaldo;
+    
+    ArrayList<SaldoDB> list =new ArrayList<SaldoDB>();
+    String[] title = {"nominal"};
+    int index = 0;
     
     public Saldo(){
-        
+        try {
+            connection=DriverManager.getConnection("jdbc:ucanaccess://"
+                    +"D:/E-Book/DDPL - Dasar Dasar Pengembangan Perangkat Lunak/INULCELL;","","");
+            
+            System.out.println("berhasil");
+            
+            stmt= connection.createStatement();
+            rsSaldo=stmt.executeQuery("SELECT * FROM saldoDB");
+            
+            while(rsSaldo.next()){
+                list.add(new SaldoDB(rsSaldo.getInt("nominal")));
+            }
+            
+        } catch (SQLException errMsg) {
+           System.out.println("ada kesalahan : "+ errMsg.getMessage());
+        }
     }
     
     /*  apabila melakukan tambah(integer), maka return true

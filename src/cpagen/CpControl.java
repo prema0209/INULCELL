a89dd93ce45cb5cbdca01ccc9d208a3b85716ece
linkherz <linkherzg@gmail.com>
2016-12-04 23:36:37 +0700

    Update CpControl.java
    berhasil tambah ke access

--- src/cpagen/CpControl.java
+++ src/cpagen/CpControl.java
@@ -1,21 +1,84 @@
-private void updateTable(){
-        Object data[][] = new Object[this.list.size()][4];
-        int x = 0;
-        for(Anggota agt : this.list) {
-            data[x][0] = agt.getNoAnggota();
-            data[x][1] = agt.getNama();
-            data[x][2] = agt.getAlamat();
-            data[x][3] = agt.getPekerjaan();
-            ++x;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cpagen;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author UsuiTakumi
+ */
+public class CpControl {
+    
+    Connection connection=null;
+    Statement stmt;
+    ResultSet rsCP;
+    
+    ArrayList<CPagenDB> list =new ArrayList<CPagenDB>();
+    String[] title = {"No ID","Nama","No. Hp"};
+    int index = 0;
+    
+    public CpControl(){
+        //initComponents();
+        
+        try {
+            connection=DriverManager.getConnection("jdbc:ucanaccess://"
+                    +"D:/E-Book/DDPL - Dasar Dasar Pengembangan Perangkat Lunak/INULCELL/inulcell.accdb;","","");
+            
+            System.out.println("berhasil connect");
+            
+            stmt= connection.createStatement();
+            rsCP=stmt.executeQuery("SELECT * FROM agen");
+            
+            while(rsCP.next()){
+                
+                list.add(new CPagenDB( //rsCP.getString("ID") ,
+                        rsCP.getString("Nama") ,
+                        rsCP.getString("NoHp")));
+            }
+        } catch (SQLException errMsg) {
+           System.out.println("ada kesalahan : "+ errMsg.getMessage());
         }
-        tblAnggota.setModel(new DefaultTableModel(data, title));
+     //   updateTable();
+     //   showData();
+    }    
+    
+    public boolean tambah(String nama, String NoHp) { 
+        String sql;
+        
+        CPagenDB newAgen = new CPagenDB();
+        newAgen.setNama(nama);
+        newAgen.setNohp(NoHp);
+        this.list.add(newAgen);
+       
+        sql = "INSERT INTO Agen (Nama, NoHp) VALUES"
+                +"('"+nama+"',"
+                + "'"+NoHp+"')";
+        
+        System.out.println(sql);
+        
+        try{
+           int berhasil=stmt.executeUpdate(sql);    
+        } catch (SQLException errMsg) {
+           System.out.println("ada kesalahan : "+ errMsg.getMessage());
+        }
+        
+        //JOptionPane.showMessageDialog(this, "Data Tersimpan", "Informasi", JOptionPane.INFORMATION_MESSAGE);
+        return true; 
     }
     
-    private void showData(){
-        Anggota agt = this.list.get(this.index);
-        this.txtNoAnggota.setText(agt.getNoAnggota());
-        this.txtNama.setText(agt.getNama());
-        //this.txtAlamat.setToolTipText(agt.getAlamat());
-        this.txtAlamat.setText(agt.getAlamat());
-        this.txtPekerjaan.setText(agt.getPekerjaan());
-    }
+    public void lihat(){ }
+    
+    public boolean hapus(String nama){ return true; } 
+}

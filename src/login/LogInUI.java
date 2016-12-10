af523b12eec626a33dc7f30f0c9448db946cb754
DhiemasRakadewa <dhiemasrd@gmail.com>
2016-12-10 19:13:34 +0700

    Update Login

--- src/login/LogInUI.java
+++ src/login/LogInUI.java
@@ -7,19 +7,54 @@
 package login;
 import java.awt.Toolkit;
 import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import menuawal.MenuAwal;
-
-
+import java.sql.PreparedStatement;
 
 public class LogInUI extends javax.swing.JFrame {
+    ArrayList<LogInDB> list = new ArrayList<>();
+    String[] title = {"User Lama", "Password Lama"};
+    //int index = 0;
 
     /**
      * Creates new form LogInUI
      */
+    Connection connection;
+     Statement stmt;
+     ResultSet rsUser;
+     PreparedStatement pst;
     private LogIn control;
     
     public LogInUI() {
+        try
+        {
+        connection = DriverManager.getConnection("jdbc:ucanaccess://"
+                + "E:/Matkul semester 3/ddppl/LogIn.accdb","", "");
+        System.out.println("Berhasil Konek");
+        
+        stmt = connection.createStatement();
+        rsUser = stmt.executeQuery("SELECT * FROM LogIn");
+        
+        
+        
+        while(rsUser.next()==true)
+        {
+            list.add(new LogInDB(rsUser.getString("Username"),
+                    rsUser.getString("Password")));
+
+        }
+        
+        
+        } catch (SQLException errMsg)
+        {
+            System.out.println("Ada Kesalahan" + errMsg.getMessage());
+        }    
         initComponents();
     }
 
